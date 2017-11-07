a10c1c23257d12612606d87c8c7d38e9afda833d
Juwan Wheatley <datwheat@gmail.com>
2017-11-07 13:10:54 -0500

    render backbutton on header conditionally

--- navigation/components/styled/HeaderContainer.js
+++ navigation/components/styled/HeaderContainer.js
@@ -9,4 +9,5 @@ export default styled.View`
   flex-direction: row;
   justify-content: space-between;
   align-items: center;
+  width: 100%;
 `;
--- screens/Home.js
+++ screens/Home.js
@@ -1,27 +1,24 @@
 import React, { Component } from "react";
 import { View, Button } from "react-native";
-import { withNavigation } from "react-navigation";
 
-export default withNavigation(
-  class HomeScreen extends Component {
-    render() {
-      return (
-        <View
-          style={{
-            flex: 1,
-            justifyContent: "center",
-            alignItems: "center",
-            backgroundColor: "papayawhip",
+export default class HomeScreen extends Component {
+  render() {
+    return (
+      <View
+        style={{
+          flex: 1,
+          justifyContent: "center",
+          alignItems: "center",
+          backgroundColor: "papayawhip",
+        }}
+      >
+        <Button
+          title="skrrrahh"
+          onPress={() => {
+            this.props.screenProps.navigation.navigate("Workout");
           }}
-        >
-          <Button
-            title="skrrrahh"
-            onPress={() => {
-              this.props.screenProps.navigation.navigate("Workout");
-            }}
-          />
-        </View>
-      );
-    }
+        />
+      </View>
+    );
   }
-);
+}
