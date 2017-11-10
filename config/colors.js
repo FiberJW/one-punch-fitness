b93f748c8a72fc0613158534889d2168f5fcefe3
Juwan Wheatley <datwheat@gmail.com>
2017-11-10 14:15:40 -0500

    add background color

--- config/colors.js
+++ config/colors.js
@@ -1,5 +1,6 @@
 export default {
   purpp: "#42002F",
   halfWhite: "rgba(2555, 255, 255, 0.5)",
-  twentyWhite: "rgba(2555, 255, 255, 0.2)"
+  twentyWhite: "rgba(2555, 255, 255, 0.2)",
+  offWhite: "#E0E0E0",
 };
