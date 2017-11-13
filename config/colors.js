9c82de1a3b592461223d7d3ff4663e4d753cc975
Juwan Wheatley <datwheat@gmail.com>
2017-11-13 15:48:25 -0500

    i like this color scheme better

--- config/colors.js
+++ config/colors.js
@@ -4,9 +4,13 @@ export default {
   halfWhite: "rgba(2555, 255, 255, 0.5)",
   twentyWhite: "rgba(2555, 255, 255, 0.2)",
   offWhite: "#E0E0E0",
+  offYellow: "#FFFEEC",
   spotiBlack: "#191414",
   fortyBlack: "rgba(0, 0, 0, 0.4)",
+  twentyBlack: "rgba(0, 0, 0, 0.2)",
   halfBlack: "rgba(0, 0, 0, 0.5)",
   heavenBlue: "#2DBDDA",
   froggo: "#39B822",
+  status: "#651FFF",
+  start: "#1DE9B6",
 };
