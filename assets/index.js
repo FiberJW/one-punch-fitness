c878e862a77b0afcb776d0fc93266d239984f728
Juwan Wheatley <datwheat@gmail.com>
2017-12-18 17:51:02 -0500

    use Expo's vector icons instead of shipping exported pngs of Feather icons

--- assets/index.js
+++ assets/index.js
@@ -4,5 +4,4 @@
  */
 
 export { default as Fonts } from "./fonts";
-export { default as Icons } from "./icons";
 export { default as Illustrations } from "./illustrations";
--- src/screens/Home/components/InfoCard/OverflowButton/styled/Icon.js
+++
@@ -1,7 +0,0 @@
-// @flow
-import styled from "styled-components/native";
-
-export default styled.Image`
-  height: 6px;
-  width: 12px;
-`;
