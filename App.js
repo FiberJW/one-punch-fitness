65735b9dd18ebd06fc3aebf1662a4986e2f14041
Juwan Wheatley <datwheat@gmail.com>
2017-12-26 19:06:14 -0500

    add sentry error tracking

--- App.js
+++ App.js
@@ -1,4 +1,8 @@
 // @flow
+import Sentry from "sentry-expo";
 import App from "./src/index.bs";
 
+Sentry.config(
+  "https://20b4ae39f24049e796732bacf3283728:48fb051e0cc24e98bc0ccfe68d75e514@sentry.io/264077"
+).install();
 export default App;
